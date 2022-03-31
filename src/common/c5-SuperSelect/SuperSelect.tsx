ef9cb242421af9d313899bfeb81d21bb54a7aa00
EvgeniyProkhorov <firerequest@gmail.com>
2022-03-31 21:40:19 +0300

    SuperComponents

---
+++ src/common/c5-SuperSelect/SuperSelect.tsx
@@ -0,0 +1,34 @@
+import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+
+type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
+
+type SuperSelectPropsType = DefaultSelectPropsType & {
+    options?: string[]
+    onChangeOption?: (option: string) => void
+}
+
+const SuperSelect: React.FC<SuperSelectPropsType> = (
+    {
+        options,
+        onChange, onChangeOption,
+        ...restProps
+    }
+) => {
+    const mappedOptions: JSX.Element[] | undefined = options?.map((el, i) => {
+        return <option key={`${el}_${i}`} value={el}>{el}</option>
+    }); // map options with key
+
+    const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
+    }
+
+    return (
+        <select onChange={onChangeCallback} {...restProps}>
+            {mappedOptions}
+        </select>
+    )
+}
+
+export default SuperSelect
