0ef5750d7b8010e1d784c56842f423ae37ed0560
EvgeniyProkhorov <firerequest@gmail.com>
2022-03-31 22:15:48 +0300

    HW14

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Login} from "./Components/Login/Login";
-import {NewPasswordIput} from "./Components/NewPasswordInput/NewPasswordIput";
+import {NewPassword} from "./Components/NewPasswordInput/NewPasswordIput";
 import {PageNotFound} from "./Components/PageNotFound/PageNotFound";
 import {PasswordRecovery} from "./Components/PasswordRecovery/PasswordRecovery";
 import {Profile} from "./Components/Profile/Profile";
@@ -15,7 +15,7 @@ function App() {
         <div className="App">
           <Routes>
               <Route path={'login'} element={<Login/>}/>
-              <Route path={'new-password'} element={<NewPasswordIput/>}/>
+              <Route path={'new-password'} element={<NewPassword/>}/>
               <Route path={'*'} element={<PageNotFound/>}/>
               <Route path={'pass-recovery'} element={<PasswordRecovery/>}/>
               <Route path={'profile'} element={<Profile/>}/>
--- src/Components/NewPasswordInput/NewPasswordIput.tsx
+++ src/Components/NewPasswordInput/NewPasswordIput.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const NewPasswordIput = () => {
+export const NewPassword = () => {
     return(
         <div>
             New Password
